9db26f230cd14c8e9c2c5b4bba1c662a5683760b
KDA <kuda74ru@gmail.ru>
2023-08-26 13:00:25 +0500

    add Example04

---
+++ Examples/Example04/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задайте двумерный массив. Найдите сумму элементов, находящихся на главной диагонали (с индексами (0,0); (1;1) и т.д.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Сумма элементов главной диагонали: 1+9+2 = 12
+
+Console.WriteLine("Введите первую размерность");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите вторую размерность");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+
+int[,] matrix = new int[rows, columns];
+
+int sum = 0;
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        matrix[i, j] = new Random().Next(0,10);
+        Console.Write(matrix[i, j] + " ");
+
+        if (i == j)
+        {
+            sum += matrix[i,j];
+        }
+    }
+Console.WriteLine();
+}
+Console.WriteLine("сумма по главной диагонали = " + sum);
