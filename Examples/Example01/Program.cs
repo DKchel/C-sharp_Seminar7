9df881e6aa98b52b90ec628e741f5042f6d500c1
KDA <kuda74ru@gmail.ru>
2023-08-26 12:27:29 +0500

    add Example01

--- Examples/Example01/Program.cs
+++ Examples/Example01/Program.cs
@@ -14,3 +14,13 @@ int columns = Convert.ToInt32(Console.ReadLine());
 
 int[,] matrix = new int[rows, columns];
 
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        matrix[i, j] = new Random().Next(0,10);
+        Console.Write(matrix[i, j] + " ");
+    }
+Console.WriteLine();
+}
+
