c263c5ed095374ed4c1b806ddb2181d7ca6738c2
KDA <kuda74ru@gmail.ru>
2023-08-26 12:13:54 +0500

    initial commit

---
+++ Examples/Example01/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными целыми числами.
+// m = 3, n = 4.
+// 1 4 8 19
+// 5 -2 33 -2
+// 77 3 8 1
+
+
+Console.WriteLine("Введите первую размерность");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите вторую размерность");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+
+int[,] matrix = new int[rows, columns];
+
